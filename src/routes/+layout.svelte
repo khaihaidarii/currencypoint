662fa5fb864fc410b24adab87ccf33a8c2d6bc07
= <khaistudys@gmail.com>
2024-01-24 21:58:36 +1100

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.pcss';
+</script>
+
+<main class="grid h-screen w-screen place-items-center">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex max-w-[345px] flex-col gap-4 text-center">
+	<p>- currencypoint.com.au -</p>
+	<h1 class="text-4xl font-bold uppercase">Coming Soon!</h1>
+	<p>
+		Contact us at <i>info@currencypoint.com.au</i> for information about launch or any other questions
+	</p>
+</div>
