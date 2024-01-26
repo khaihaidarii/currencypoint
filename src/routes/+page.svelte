c6849cf4fb9f4ce5b2a18ae05cb4d4e084dcc860
khaihaidarii <khaihaidari10@gmail.com>
2024-01-26 18:06:55 +1100

    added title and desc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<svelte:head>
+	<title>Currency Point - Foreign Currency Exchange - Coming soon...</title>
+	<meta
+		name="description"
+		content="Currency Point, your local foreign currency exchange, is coming soon! For more information, please email us at info@currencypoint.com.au."
+	/>
+</svelte:head>
+
 <div class="flex max-w-[345px] flex-col gap-4 text-center">
 	<p>- currencypoint.com.au -</p>
 	<h1 class="text-4xl font-bold uppercase">Coming Soon!</h1>
